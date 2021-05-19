8af23d73ad3e61251e996d261a3e80dd6802e113
guilly <swimmerg2799@gmail.com>
2021-05-19 13:37:50 -0500

    agregue lineas en el manifest y conexion de entanas entre reservesacion ticket y menu principal(corregir)

--- First/app/src/main/java/com/example/myapplication/Ticket.kt
+++ First/app/src/main/java/com/example/myapplication/Ticket.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.layout_iniciosesion.*
 import kotlinx.android.synthetic.main.layout_misestancias.*
+import kotlinx.android.synthetic.main.layout_ticket.*
 
-class MisEstancias : AppCompatActivity() {
+class Ticket : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_misestancias);
+        setContentView(R.layout.layout_ticket);
 
-        this.button9.setOnClickListener(){
-            Toast.makeText(this, "Regresar al menu principal.", Toast.LENGTH_SHORT).show()
+        this.button17.setOnClickListener(){
+            Toast.makeText(this, "Regresar al menu principal", Toast.LENGTH_SHORT).show()
             val menuActivity = Intent(applicationContext, DashboardActivity::class.java)
             startActivity(menuActivity)
         }
     }
-
 }
