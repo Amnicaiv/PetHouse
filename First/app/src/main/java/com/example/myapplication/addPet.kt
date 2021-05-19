3cb4b5df1a0e913c12cd75979c491a6db7cec083
guilly <swimmerg2799@gmail.com>
2021-05-19 15:05:31 -0500

    agregue dos ventanas que son, la de editpet y la de createpet y ademas ya tiene una conexion

---
+++ First/app/src/main/java/com/example/myapplication/addPet.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_reservacion_checkout.*
+
+class addPet : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_createpet);
+
+        }
+
+
+    }
