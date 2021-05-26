b9f5adf7044cfed2fb8a0c11e674f27d0509d93c
Amnicaiv <44104222+Amnicaiv@users.noreply.github.com>
2021-05-26 12:10:44 -0500

    Se arreglo las pantallas de login y signup (bug visual) y se agrego estilo a la pantalla de petinfo

--- First/app/src/main/java/com/example/myapplication/PetInfiActivity.kt
+++ First/app/src/main/java/com/example/myapplication/PetInfiActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.layout_petinfo.*
 
@@ -14,6 +16,29 @@ class PetInfiActivity : AppCompatActivity(){
 
         this.editTextTextPersonName6.setText(petName)
 
+        var mLastClickTime = 0.0
+
+        var doublePressPrev = false
+
+        this.button9.setOnClickListener(){
+            doublePressPrev = SystemClock.elapsedRealtime() - mLastClickTime < 1000
+            mLastClickTime = SystemClock.elapsedRealtime().toDouble();
+
+            if(!doublePressPrev){
+                finish()
+            }
+        }
+
+
+
+        this.button11.setOnClickListener(){
+            doublePressPrev = SystemClock.elapsedRealtime() - mLastClickTime < 1000
+            mLastClickTime = SystemClock.elapsedRealtime().toDouble()
+
+            if(!doublePressPrev){
+                finish()
+            }
+        }
 
 
     }
